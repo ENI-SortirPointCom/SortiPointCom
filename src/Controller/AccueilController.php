ccc0cba56e64a49e68ed90b4111a57cd5f2bd2ac
acoste2019 <Celno1211!>
2020-02-21 16:38:23 +0100

    actions dans liste sortie

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,29 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\Site;
 use App\Entity\Sortie;
+use App\Form\SortieFilterType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     /**
      * @Route("/accueil", name="accueil")
      */
-    public function index(EntityManagerInterface $em)
+    public function index(Request $request,EntityManagerInterface $em)
     {
         $sorties = $em->getRepository(Sortie::class)->findAll();
+        $sites = $em->getRepository(Site::class)->findAll();
+        $form = $this->createForm(SortieFilterType::class);
+        $form->handleRequest($request);
 
 
 
 
         return $this->render('accueil/index.html.twig', [
             "sorties" => $sorties,
+            "sites" => $sites,
+            "controller_name" => 'Accueil',
+            'sortieFilterForm' => $form->createView(),
             /**
              * pour passer en parametre de l'extension twig pesonnalisée
              */
             'user' => $this->getUser()
         ]);
     }
+
+
+//public function formSearchSortie(Request $request)
+//{
+//    $form = $this->createForm(SortieFilterType::class);
+//    $form->handleRequest($request);
+//
+//    return $this->render('accueil/sortieFilter.html.twig', [
+//        'sortieFilterForm' => $form->createView()
+//    ]);
+//
+//
+//}
 
 }
---
+++ src/Form/SortieFilterType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieFilterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('dateHeureDebut')
+            ->add('heureFin')
+            ->add('dateLimitInscription')
+            ->add('lieu')
+            ->add('user')
+            ->add('participant')
+            ->add('organisateur')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
